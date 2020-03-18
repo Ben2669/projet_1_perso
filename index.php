e21a25574e9794740fdb5ef1fa637ff4a576a347
Ben2669 <benjamin.moniotte@gmail.com>
2020-03-18 15:20:06 +0100

    adding a _header.php file and switching index.html into index.php to include header more easily

--- index.php
+++ index.php
@@ -35,33 +35,7 @@
 
         </div>
 
-	<header>
-        <section class="logo">
-            <a href="index.html">
-                <img class ="logo_smartphone" alt="logo_retro_invaders_smartphone" src="Images/Autres/blog-loco-green.png" height="110" width="auto">
-                <div class="logo-header"></div>
-            </a>
-
-        </section>
-
-        <section class="menu_burger_container">
-            <article id="burger-menu-click" class="menu_burger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-
-            </article>
-        </section>
-
-        <nav>
-            <ul>
-                <li><a href="plateformes.html">Platforms</a></li>
-                <li><a href="about_us.html">About Us</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-        </nav>
-
-    </header>
+    <?php include("_header.php"); ?>
 
 	<main>
 		<section class="banner-homepage">
@@ -222,7 +196,7 @@
                 </a>
             </article>
             <article class="hidden">
-                <a href="index.html">
+                <a href="index.php">
                     <div class="logo_retro_invaders">
                         <img class="logo_desktop" alt="logo_retro_invaders_desktop" src="Images/Autres/blog-loco-green.png" height="150" width="auto">
                     </div>
