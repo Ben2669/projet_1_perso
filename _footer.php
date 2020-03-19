edc44bc71cf465c97ad806d053eb14645493fed1
Ben2669 <benjamin.moniotte@gmail.com>
2020-03-19 13:54:06 +0100

    adding head section to _header.php, creating burger.js and linking it to _footer.php

--- _footer.php
+++ _footer.php
@@ -76,20 +76,8 @@
     </section>
 </footer>
 
-<script type="text/javascript">
-    jQuery(function(){
-        var div = jQuery("#burger-menu-click"), div = jQuery("#nav-mobile-container"), div = jQuery(".close-menu-mobile");
-        jQuery("#burger-menu-click").click(function(){
-            jQuery("#burger-menu-click").toggleClass ("no-content");
-            jQuery("#nav-mobile-container").toggleClass ("no-content");
-            jQuery(".close-menu-mobile").toggleClass ("no-content");
-        });
-        jQuery(".close-menu-mobile").click(function(){
-            jQuery("#burger-menu-click").toggleClass ("no-content");
-            jQuery("#nav-mobile-container").toggleClass ("no-content");
-            jQuery(".close-menu-mobile").toggleClass ("no-content");
-        });
-    });
+<script type="text/javascript" src="burger.js">
+
 </script>
 
 </body>
