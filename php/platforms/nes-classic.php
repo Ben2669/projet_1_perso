150d42036d254d4ef2de724be42a69b0497d3f1a
Ben2669 <benjamin.moniotte@gmail.com>
2020-03-20 15:13:10 +0100

    including _header.php and _footing.php on every platforms file. Calling page title and page proper css dynamically throught php.

--- php/platforms/nes-classic.php
+++ php/platforms/nes-classic.php
@@ -1,39 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Retro Invader</title>
-    <meta lang="en">
-    <meta name="title" content="Retro Invaders">
-    <meta charset="UTF-8">
-    <meta name="description" content="Retro Invaders helps you to find your favorite retro games">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" type="text/css" href="plateformes_style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900|Roboto:300,400,700,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="header_style.css" />
-    <link rel="stylesheet" type="text/css" href="footer_style.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
-    <link rel='stylesheet' id='thb-self-hosted-1-css' href="gilroy/gilroy.css">
-</head>
-<body>
-<div id="nav-mobile-container" class="nav-mobile-full no-content">
-    <div class="close-menu-mobile no-content">
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </div>
-    <nav class="mobile-nav">
-        <ul>
-            <a href="plateformes.php"><li>Platforms</li></a>
-            <a href="about_us.php"><li>About Us</li></a>
-            <a href="contact.php"><li>Contact</li></a>
-        </ul>
-    </nav>
-
-</div>
-
-<?php include("_header.php"); ?>
+<?php
+$title = "NES Classic";
+$css = "/css/plateformes_style.css";
+include("../_header.php"); ?>
 
 <main>
 
@@ -69,7 +37,7 @@
             </div>
 </main>
 
-<?php include("_footer.php");?>
+<?php include("../_footer.php");?>
 </main>
 
 </body>
